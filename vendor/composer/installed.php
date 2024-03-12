3181eea71e51b63cb1284c0213a60fa18eb34052
viVo4s <stas22900@mail.ru>
2024-03-12 19:43:14 +0700

    Setting up class autoloading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'stas2/htdocs',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cc1e4dd61250a5e82a1738385d3a858cf42b3c16',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'stas2/htdocs' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cc1e4dd61250a5e82a1738385d3a858cf42b3c16',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
